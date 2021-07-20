dcb1aa7b2a20e20cc335de8f0eeef7f708fae75c
Gordon <gmeccles@gmail.com>
2021-07-20 13:45:16 -0400

    Add text, add form, log submit

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
-from flask import Flask, render_template
+from logging.config import dictConfig
+
+from flask import Flask, render_template, request
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s][%(levelname)s][%(module)s] %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/accept', methods=['POST'])
+def accept():
+    name = request.form.get("name")
+    email = request.form.get("email")
+    app.logger.info(f'ACCEPT_EVENT: email="{email}" name="{name}"')
+    return {'success': True}
+
+
+@app.route('/', methods=['GET'])
 def invite():
     return render_template('invite.html')
--- static/fade.js
+++ static/fade.js
@@ -1,10 +1,10 @@
 
-
-window.onload = function() {
-
+const addFadeEffect = () => {
   const elIdToTarget = {
     '#welcome': 0,
     '#info': 200,
+    '#warning': 400,
+    '#final': 600,
   };
   let ctr = 0;
 
@@ -12,7 +12,7 @@ window.onload = function() {
     const normalizedDelta = delta / Math.abs(delta);
     ctr += (2 * normalizedDelta)
     ctr = Math.max(0, ctr)
-    ctr = Math.min(200, ctr)
+    ctr = Math.min(600, ctr)
 
     for (const [elId, target] of Object.entries(elIdToTarget)) {
       const el = document.querySelector(elId);
@@ -25,9 +25,38 @@ window.onload = function() {
   document.addEventListener('DOMMouseScroll', function(e) {
     doFade(e.detail);
   }, false);
+
   // Chrome, Safari
   document.addEventListener('mousewheel', function(e) {
     doFade(-1 * e.wheelDelta);
   }, false);
+};
 
+const addSubmitHandler = () => {
+  const acceptForm = document.getElementById('accept');
+  acceptForm.addEventListener('submit', e => {
+    e.preventDefault();
+    fetch(
+        '/accept',
+        {
+          body: new FormData(e.target),
+          method: 'POST'
+        }
+    ).then(d =>
+        d.json()
+    ).then(res => {
+      if (res['success']) {
+        const thankYou = document.getElementById('thank-you');
+        acceptForm.style.display = 'none';
+        thankYou.style.display = 'block';
+      }
+    }).catch(err =>
+        console.log(err)
+    )
+  })
+}
+
+window.onload = function() {
+  addFadeEffect();
+  addSubmitHandler();
 };
