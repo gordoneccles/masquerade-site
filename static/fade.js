08ef64a3659f27945e1f5d452c83580938c12c2b
Gordon <gmeccles@gmail.com>
2021-07-25 14:11:30 -0400

    Handle fade scroll on mobile, disable overscroll

--- static/fade.js
+++ static/fade.js
@@ -1,62 +1,95 @@
-
-const addFadeEffect = () => {
-  const elIdToTarget = {
+const OPACITY_POINTS = {
     '#welcome': 0,
     '#info': 200,
     '#warning': 400,
     '#final': 600,
-  };
-  let ctr = 0;
+};
+let opacityPoint = 0;
 
-  const doFade = (delta) => {
-    const normalizedDelta = delta / Math.abs(delta);
-    ctr += (2 * normalizedDelta)
-    ctr = Math.max(0, ctr)
-    ctr = Math.min(600, ctr)
-
-    for (const [elId, target] of Object.entries(elIdToTarget)) {
-      const el = document.querySelector(elId);
-      const opacity = (100 - Math.abs(target - ctr)) / 100
-      el.style.opacity = '' + opacity;
+const updateOpacities = () => {
+    for (const [elId, target] of Object.entries(OPACITY_POINTS)) {
+        const el = document.querySelector(elId);
+        const opacity = (100 - Math.abs(target - opacityPoint)) / 100;
+        el.style.opacity = '' + opacity;
     }
-  }
+};
+const setOpacity = val => {
+    opacityPoint = Math.min(600, Math.max(0, val));
+    updateOpacities();
+};
 
-  // Firefox
-  document.addEventListener('DOMMouseScroll', function(e) {
-    doFade(e.detail);
-  }, false);
+const addDomScrollHandler = () => {
+    // Firefox
+    document.addEventListener('DOMMouseScroll', function (e) {
+        const delta = e.detail;
+        const normalizedDelta = delta / Math.abs(delta);
+        setOpacity(opacityPoint + 2 * normalizedDelta);
+    }, false);
+};
 
-  // Chrome, Safari
-  document.addEventListener('mousewheel', function(e) {
-    doFade(-1 * e.wheelDelta);
-  }, false);
+const addMousewheelHandler = () => {
+    // Chrome, Safari
+    document.addEventListener('mousewheel', function (e) {
+        const delta = -1 * e.wheelDelta;
+        const normalizedDelta = delta / Math.abs(delta);
+        setOpacity(opacityPoint + 2 * normalizedDelta);
+    }, false);
+};
+
+const addTouchHandlers = () => {
+    let opacityStart;
+    let startY;
+    document.addEventListener(
+        "touchstart",
+        e => {
+            let t = e.changedTouches[0];
+            startY = t.clientY;
+            opacityStart = opacityPoint;
+            console.log(`capture opacityStart to ${opacityPoint}`)
+        },
+        false
+    );
+    document.addEventListener(
+        "touchmove",
+        e => {
+            setOpacity(opacityStart + Math.floor((startY - e.changedTouches[0].clientY) / 4))
+        },
+        false
+    );
+    // document.addEventListener(
+    //     "touchend",
+    //     e => startY = null,
+    //     false);
 };
 
 const addSubmitHandler = () => {
-  const acceptForm = document.getElementById('accept');
-  acceptForm.addEventListener('submit', e => {
-    e.preventDefault();
-    fetch(
-        '/accept',
-        {
-          body: new FormData(e.target),
-          method: 'POST'
-        }
-    ).then(d =>
-        d.json()
-    ).then(res => {
-      if (res['success']) {
-        const thankYou = document.getElementById('thank-you');
-        acceptForm.style.display = 'none';
-        thankYou.style.display = 'block';
-      }
-    }).catch(err =>
-        console.log(err)
-    )
-  })
+    const acceptForm = document.getElementById('accept');
+    acceptForm.addEventListener('submit', e => {
+        e.preventDefault();
+        fetch(
+            '/accept',
+            {
+                body: new FormData(e.target),
+                method: 'POST'
+            }
+        ).then(d =>
+            d.json()
+        ).then(res => {
+            if (res['success']) {
+                const thankYou = document.getElementById('thank-you');
+                acceptForm.style.display = 'none';
+                thankYou.style.display = 'block';
+            }
+        }).catch(err =>
+            console.log(err)
+        )
+    })
 }
 
-window.onload = function() {
-  addFadeEffect();
-  addSubmitHandler();
+
+window.onload = function () {
+    addDomScrollHandler();
+    addMousewheelHandler();
+    addTouchHandlers();
+    addSubmitHandler();
 };
